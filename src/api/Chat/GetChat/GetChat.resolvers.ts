ab67c5917fd46b9dd4f59380bc0c418bbe0d3a90
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-11 21:12:42 +0900

    ??

--- src/api/Chat/GetChat/GetChat.resolvers.ts
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -3,53 +3,58 @@ import { GetChatQueryArgs, GetChatResponse } from "src/types/graph";
 import Chat from "../../../entities/Chat";
 import User from "../../../entities/User";
 
-const resolvers : Resolvers = {
-    Query: {
-        GetChat: async (_, args: GetChatQueryArgs, context) : Promise<GetChatResponse> => {
-            const user : User = context.req.user;
-            const {chatId} = args;
-            if(!user) {
-                return {
-                    ok:false,
-                    error: "You have to login",
-                    chat: null
-                }
-            }
+const resolvers: Resolvers = {
+  Query: {
+    GetChat: async (
+      _,
+      args: GetChatQueryArgs,
+      context
+    ): Promise<GetChatResponse> => {
+      const user: User = context.req.user;
+      const { chatId } = args;
+      if (!user) {
+        return {
+          ok: false,
+          error: "You have to login",
+          chat: null
+        };
+      }
 
-
-            try{
-                const chat : Chat | undefined = await Chat.findOne({id:chatId}, {relations: ["messages"]});
-                if(chat){
-                    if(chat.passengerId === user.id || chat.driverId === user.id){
-                        return {
-                            ok:true,
-                            error: null,
-                            chat
-                        }
-                    }else {
-                        return {
-                            ok:false,
-                            error: "Not authorized to see this chat",
-                            chat: null
-                        }
-                    }
-                }else {
-                    return {
-                        ok:false,
-                        error: "There is no chat",
-                        chat:null
-                    }
-                }
-                
-            }catch(error) {
-                return {
-                    ok:false,
-                    error: error.message,
-                    chat:null
-                }
-            }
+      try {
+        const chat: Chat | undefined = await Chat.findOne(
+          { id: chatId },
+          { relations: ["messages", "messages.user"] }
+        );
+        if (chat) {
+          if (chat.passengerId === user.id || chat.driverId === user.id) {
+            return {
+              ok: true,
+              error: null,
+              chat
+            };
+          } else {
+            return {
+              ok: false,
+              error: "Not authorized to see this chat",
+              chat: null
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "There is no chat",
+            chat: null
+          };
         }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          chat: null
+        };
+      }
     }
-}
+  }
+};
 
 export default resolvers;
